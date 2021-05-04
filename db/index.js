eb4528b0edf247a5cab93577191a22466bdecec1
loic <loic.des972@gmail.com>
2021-05-04 09:25:33 +0100

    fix connection

--- db/index.js
+++ db/index.js
@@ -3,40 +3,34 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://pizzaecalzones:pizzaecalzones@pizzaecalzones.w16cm.mongodb.net/pizzaecalzones?retryWrites=true&w=majority";
 
 let connection;
-
-exports.getConnection = async (database, collection) => {
-    try {
-        if (connection) {
-            return connection.db(database).collection(collection);
-        } else {
-            connection = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            connection = await connection.connect();
-            return connection.db(database).collection(collection);
-        }
-    } catch (e) {
-        console.log('Erreur in getConnection ' + e.message);
-     }
-};
+try {
+    connection = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    connection.connect().then(value => {
+        connection = value;
+    });
+}catch(err) {
+    console.log(err);
+}
 
 exports.get = async (database, collection, param = {}) => {
-    const col = await this.getConnection(database, collection);
+    const col = connection.db(database).collection(collection);
     return await col.find(param).toArray();
 }
 
 exports.deleteMany = async (database, collection, param = {}) => {
-    const col = await this.getConnection(database, collection);
+    const col = connection.db(database).collection(collection);
     return await col.deleteMany(param);
 }
 
 exports.insertOne = async (database, collection, param) => {
-    const col = await this.getConnection(database, collection);
+    const col = connection.db(database).collection(collection);
     return await col.insertOne(param);
 }
 exports.insertMany = async (database, collection, param) => {
-    const col = await this.getConnection(database, collection);
+    const col = connection.db(database).collection(collection);
     return await col.insertMany(param);
 }
 exports.findOneAndUpdate = async (database, collection, param, set) => {
-    const col = await this.getConnection(database, collection);
+    const col = connection.db(database).collection(collection);
     return await col.findOneAndUpdate(param, {$set: set});
 }
